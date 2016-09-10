3a4039e13b1faa551f137995e2cab95d417f5b19
drowl <drowl@23.me.uk>
2016-09-10 02:08:36 +0100

    Sort the missions in view list using alpha num sort function Sorting the "missions in view" list makes it much simpler when working on banner missions I have aded a function to do this based on alphanum.js by Brian Huisman

--- plugins/missions.user.js
+++ plugins/missions.user.js
@@ -73,6 +73,64 @@ var timeToRemaining = function(t) {
 	data = data.replace('00m', '');
 	return data.trim();
 };
+ /* alphanum.js (C) Brian Huisman
+ * Based on the Alphanum Algorithm by David Koelle
+ * The Alphanum Algorithm is discussed at http://www.DaveKoelle.com
+ *
+ * Edited to work with an array of mission objects (sorting them on title)
+ * for use with the iitc missions plugin
+ *
+ * Distributed under same license as original
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+/* ********************************************************************
+ * Alphanum sort() function version - case sensitive
+ *  - Slower, but easier to modify for arrays of objects which contain
+ *    string properties
+ *
+ */
+var sort_alphanum_title = function(a, b) {
+  function chunkify(t) {
+    var tz = [];
+    var x = 0, y = -1, n = 0, i, j;
+
+    while (i = (j = t.charAt(x++)).charCodeAt(0)) {
+      var m = (i == 46 || (i >=48 && i <= 57));
+      if (m !== n) {
+        tz[++y] = "";
+        n = m;
+      }
+      tz[y] += j;
+    }
+    return tz;
+  }
+
+  var aa = chunkify(a.title);
+  var bb = chunkify(b.title);
+
+  for (x = 0; aa[x] && bb[x]; x++) {
+    if (aa[x] !== bb[x]) {
+      var c = Number(aa[x]), d = Number(bb[x]);
+      if (c == aa[x] && d == bb[x]) {
+        return c - d;
+      } else return (aa[x] > bb[x]) ? 1 : -1;
+    }
+  }
+  return aa.length - bb.length;
+};
 window.plugin.missions = {
 	// 3 days.
 	missionCacheTime: 3 * 24 * 3600 * 1E3,
@@ -256,6 +314,7 @@ window.plugin.missions = {
 				}
 				return;
 			}
+			missions.sort(sort_alphanum_title);
 			callback(missions);
 		}, function(error) {
 			console.error('Error loading missions in bounds', arguments);
